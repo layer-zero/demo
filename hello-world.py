1a9230c229dcef80c105e2b65380aeae4a38bb07
Tom Lijnse <tom.lijnse@layerzero.nl>
2022-05-26 10:36:22 +0200

    Fixed errors

--- hello-world.py
+++ hello-world.py
@@ -4,8 +4,8 @@
 
 print('Hello World!')
 
-# Let's raise some exception:
-# raise Exception('Here I am, brain the size of a planet, and they tell me to raise an exception...')
-#
 # Let's create a syntax error:
-print 'Hello World'
+# print 'Hello World'
+#
+# # Let's raise some exception:
+# raise Exception('Here I am, brain the size of a planet, and they tell me to raise an exception...')
