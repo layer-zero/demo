afbec0596ef91ca62bbd39ea9354c3da09b016ac
Tom Lijnse <tom.lijnse@layerzero.nl>
2022-06-02 13:59:03 +0200

    Added print statement

--- hello-world.py
+++ hello-world.py
@@ -5,7 +5,7 @@
 print('Hello World!')
 
 # Let's create a syntax error:
-# print 'Hello World'
+print 'Hello World'
 #
 # # Let's raise some exception:
 # raise Exception('Here I am, brain the size of a planet, and they tell me to raise an exception...')
